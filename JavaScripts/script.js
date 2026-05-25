a15c6b3ab78bbd64f461d09401d6e649f2ba19a0
Boitumelo274 <2878260@students.wits.ac.za>
2026-05-25 21:06:40 +0200

    create separate css files for each html page

---
+++ JavaScripts/script.js
@@ -0,0 +1,11 @@
+//===================================
+//JavaScript Navigation for each page
+const navigation = document.querySelector("#nav");
+
+navigation.innerHTML = `
+ <nav class="nav-links">
+<a href="../index.html">Home</a>
+<a href="../Pages/about.html">About Me</a>
+<a href="../Pages/music-hiphop-culture.html">Music & Hip-Hop Culture</a>
+<a href="../Pages/contact.html">Contact</a></nav>
+`;
