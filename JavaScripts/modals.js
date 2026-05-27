1109af9cfef54f4c86b56285e9af617cf0419900
Boitumelo274 <2878260@students.wits.ac.za>
2026-05-27 22:35:06 +0200

    Created the influence album  section /added appropriate images

--- JavaScripts/modals.js
+++ JavaScripts/modals.js
@@ -58,4 +58,67 @@ const generatArtistCards = (favouriteArtists) => {
   });
 };
 
+//Calling the generateAtristCards to creat artist cards
 generatArtistCards(favouriteArtists);
+
+//Inspirational Albums
+const albumCovers = [
+  {
+    id: 1,
+    albumName: "2014 Forest Hills Drive-J.Cole",
+    imageUrl: "../Images/album-cover1.png",
+    imagePlaceHolderText: "Image1",
+  },
+  {
+    id: 2,
+    albumName: "DS4Ever-Gunna",
+    imageUrl: "../Images/album-cover2.png",
+    imagePlaceHolderText: "Image2",
+  },
+  {
+    id: 3,
+    albumName: "So Much Fun-Young Thug",
+    imageUrl: "../Images/album-cover3.png",
+    imagePlaceHolderText: "Image3",
+  },
+  {
+    id: 4,
+    albumName: "The Forever Story-JID",
+    imageUrl: "../Images/album-cover4.png",
+    imagePlaceHolderText: "Image4",
+  },
+  {
+    id: 5,
+    albumName: "Scorpion-Drake",
+    imageUrl: "../Images/album-cover5.png",
+    imagePlaceHolderText: "Image5",
+  },
+];
+
+const generateAlbumCards = (albumCovers) => {
+  const albumContainer = document.querySelector(".album-card-container");
+
+  albumContainer.innerHTML = "";
+
+  albumCovers.map((albumCover) => {
+    const albumCard = document.createElement("div");
+    albumCard.classList.add("album-card");
+    albumCard.setAttribute("album-cover-data-id", albumCover.id);
+
+    const albumCoverImage = document.createElement("img");
+    albumCoverImage.classList.add("album-cover");
+    albumCoverImage.src = albumCover.imageUrl;
+    albumCoverImage.alt = albumCover.imagePlaceHolderText;
+
+    const albumCoverName = document.createElement("h3");
+    albumCoverName.textContent = albumCover.albumName;
+
+    albumCard.appendChild(albumCoverImage);
+    albumCard.appendChild(albumCoverName);
+
+    albumContainer.appendChild(albumCard);
+  });
+};
+
+// generating album cover cards with names
+generateAlbumCards(albumCovers);
