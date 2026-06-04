8b2191c31cda6462a1fcedf5f53c4f2225b4966e
Boitumelo274 <2878260@students.wits.ac.za>
2026-06-05 01:06:56 +0200

    Testing path issues for the 404 error

--- JavaScripts/global-script.js
+++ JavaScripts/global-script.js
@@ -3,10 +3,10 @@ const navigation = document.querySelector("#nav");
 
 navigation.innerHTML = `
  <nav class="nav-links">
-<a href="index.html">Home</a>
-<a href="Pages/about.html">About Me</a>
-<a href="Pages/music-hiphop-culture.html">Music & Hip-Hop Culture</a>
-<a href="Pages/contact.html">Contact</a></nav>
+<a href="../index.html">Home</a>
+<a href="../Pages/about.html">About Me</a>
+<a href="../Pages/music-hiphop-culture.html">Music & Hip-Hop Culture</a>
+<a href="../Pages/contact.html">Contact</a></nav>
 `;
 
 //Hover highlight effect for current page
