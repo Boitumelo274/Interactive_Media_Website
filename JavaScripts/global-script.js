858d7dbf9d356fe8ab5905bbc387da09b1558e80
Boitumelo274 <2878260@students.wits.ac.za>
2026-06-05 00:27:15 +0200

    Fixing the path issue I have with github pages

--- JavaScripts/global-script.js
+++ JavaScripts/global-script.js
@@ -3,10 +3,10 @@ const navigation = document.querySelector("#nav");
 
 navigation.innerHTML = `
  <nav class="nav-links">
-<a href="../index.html">Home</a>
-<a href="../Pages/about.html">About Me</a>
-<a href="../Pages/music-hiphop-culture.html">Music & Hip-Hop Culture</a>
-<a href="../Pages/contact.html">Contact</a></nav>
+<a href="/index.html">Home</a>
+<a href="/Pages/about.html">About Me</a>
+<a href="/Pages/music-hiphop-culture.html">Music & Hip-Hop Culture</a>
+<a href="/Pages/contact.html">Contact</a></nav>
 `;
 
 //Hover highlight effect for current page
@@ -25,10 +25,10 @@ const menuBtn = document.querySelector(".menu-btn");
 const mobileMenu = document.querySelector("#mobile-menu");
 
 mobileMenu.innerHTML = `
-<a href="../index.html">Home</a>
-<a href="../Pages/about.html">About Me</a>
-<a href="../Pages/music-hiphop-culture.html">Music & Hip-Hop Culture</a>
-<a href="../Pages/contact.html">Contact</a></nav>
+<a href="/index.html">Home</a>
+<a href="/Pages/about.html">About Me</a>
+<a href="/Pages/music-hiphop-culture.html">Music & Hip-Hop Culture</a>
+<a href="/Pages/contact.html">Contact</a></nav>
 `;
 
 //About Page collapsible content
