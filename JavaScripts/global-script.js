76d297030f35f38cd948e932759ba70827a6711c
Boitumelo274 <2878260@students.wits.ac.za>
2026-06-05 01:44:47 +0200

    404 error path fixes 4

--- JavaScripts/global-script.js
+++ JavaScripts/global-script.js
@@ -4,9 +4,9 @@ const navigation = document.querySelector("#nav");
 navigation.innerHTML = `
  <nav class="nav-links">
 <a href="index.html">Home</a>
-<a href="Pages/about.html">About Me</a>
-<a href="Pages/music-hiphop-culture.html">Music & Hip-Hop Culture</a>
-<a href="Pages/contact.html">Contact</a></nav>
+<a href="about.html">About Me</a>
+<a href="music-hiphop-culture.html">Music & Hip-Hop Culture</a>
+<a href="contact.html">Contact</a></nav>
 `;
 
 //Hover highlight effect for current page
